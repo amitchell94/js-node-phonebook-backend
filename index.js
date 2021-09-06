56f8f5544afb5cb1c86361dcb17d020569ebd54b
Andy Mitchell <andymitchell1@live.co.uk>
2021-09-06 18:34:24 +0200

    Added Post and delete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "id": 1,
@@ -30,12 +32,49 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
     if (person) {
         response.json(person)
     } else {
         response.status(404).end()
     }
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    console.log(request.body)
+    const body = request.body
+    if (!body || !body.name || !body.number) {
+        return response.status(400).json({
+            error: `content missing`
+        })
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: `Name already exists`
+        })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 app.get('/info', (request, response) => {
