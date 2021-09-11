64ae861601567de6e117e7c4bfa188564ed6ecd7
Andy Mitchell <andymitchell1@live.co.uk>
2021-09-11 18:14:00 +0200

    added monggo db

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.xfubv.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+console.log(process.argv.length, "args")
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+	number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+if (process.argv.length > 3) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+
+    person.save().then(result => {
+        console.log(`person ${name} with number ${number} added`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
+
